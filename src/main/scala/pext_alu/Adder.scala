c73b516d5c8d1496849a1223784c9b4877a48be1
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-06-12 16:19:02 +0900

    ADD8 works i think

--- src/main/scala/pext_alu/Adder.scala
+++ src/main/scala/pext_alu/Adder.scala
@@ -6,7 +6,10 @@ import chisel3.stage.ChiselStage
 
 class Adder(xprlen: Int) extends Module {
   val io = IO(new Bundle {
-    val alu_baseIO = ALU_IO(xprlen)
+    val rs1_value = Input(UInt(xprlen.W))
+    val rs2_value = Input(UInt(xprlen.W))
+    val elen = Input(UInt(2.W)) // 00 -> 8bit, 01 -> 16bit, 10 -> 32bit
+    val out = Output(UInt(xprlen.W))
   })
 
   val e8_simd_len = xprlen / 8
@@ -16,30 +19,45 @@ class Adder(xprlen: Int) extends Module {
   val e8_rs2_vec = Wire(Vec(e8_simd_len, UInt(8.W)))
   val e8_out_with_carry_vec = Wire(Vec(e8_simd_len, UInt(9.W)))
 
+  // 0x0123_4567_89AB_CDEF => {0x01, 0x23, ..., 0xEF}
   for ((d, i) <- e8_rs1_vec.zipWithIndex) {
-    d := io.alu_baseIO.rs1_value((i + 1) * 8 - 1, i * 8)
+    d := io.rs1_value((8-i) * 8 - 1, (7-i) * 8)
   }
   for ((d, i) <- e8_rs2_vec.zipWithIndex) {
-    d := io.alu_baseIO.rs2_value((i + 1) * 8 - 1, i * 8)
+    d := io.rs2_value((8-i) * 8 - 1, (7-i) * 8)
   }
 
-  val e8 = (io.alu_baseIO.funct3 === "b000".U) && (io.alu_baseIO.funct7(2,1) === "b10".U)
-  val e16 = (io.alu_baseIO.funct3 === "b000".U) && (io.alu_baseIO.funct7(2,1) === "b00".U)
-  val e32 = (io.alu_baseIO.funct3 === "b010".U) && (io.alu_baseIO.funct7(2,1) === "b00".U)
-  val e64 = (io.alu_baseIO.funct3 === "b001".U) && (io.alu_baseIO.funct7(2,1) === "b00".U) && (xprlen == 64).asBool
+  // No readability but looks cool
+  // e8_rs1_vec.zipWithIndex.foreach(e => e._1 := io.rs1_value((8-e._2) * 8 - 1, (7-e._2) * 8))
+  // e8_rs2_vec.zipWithIndex.foreach(e => e._1 := io.rs2_value((8-e._2) * 8 - 1, (7-e._2) * 8))
+
+  val e8 = (io.elen === "b00".U)
+  val e16 = (io.elen === "b01".U)
 
   for ((d, i) <- e8_out_with_carry_vec.zipWithIndex) {
     d := Cat(false.B, e8_rs1_vec(i)) + Cat(false.B, e8_rs2_vec(i))
   }
 
-  val e16_out_with_carry_vec = Wire(Vec(e16_simd_len, UInt(16.W)))
+  val e16_out_with_carry_vec = Wire(Vec(e16_simd_len, UInt(17.W)))
   for((d,i) <- e16_out_with_carry_vec.zipWithIndex) {
-    d := Cat(e8_out_with_carry_vec(i*2-1)(7,0), 0.U(8.W)) + Cat(0.U(7.W), e8_out_with_carry_vec(i*2))
+    d := Cat(e8_out_with_carry_vec(i*2+1) + e8_out_with_carry_vec(i*2)(8), e8_out_with_carry_vec(i*2)(7,0))
+  }
+
+  val e32_out_with_carry_vec = Wire(Vec(e32_simd_len, UInt(33.W)))
+  for((d,i) <- e32_out_with_carry_vec.zipWithIndex) {
+    d := Cat(e16_out_with_carry_vec(i*2+1) + e16_out_with_carry_vec(i*2)(16), e16_out_with_carry_vec(i*2)(15,0))
   }
 
   when(e8) {
-    io.alu_baseIO.out := Cat(e8_out_with_carry_vec.map(i => i(7,0)))
-  } elsewhen(e16) {
-
+    io.out := Cat(e8_out_with_carry_vec.map(i => i(7,0)))
+  } .elsewhen(e16) {
+    io.out := Cat(e16_out_with_carry_vec.map(i => i(15,0)))
+  } .otherwise {
+    io.out := (if(xprlen != 64) 0.U(xprlen.W) else Cat(e32_out_with_carry_vec.map(i => i(31,0))))
   }
+}
+
+object Adder extends App {
+  def apply(xprlen: Int): Adder = if(xprlen == 32 || xprlen == 64) new Adder(xprlen) else throw new Exception("xprlen is not 32 or 64\n")
+  (new ChiselStage).emitVerilog(apply(64))
 }
---
+++ src/test/scala/pext_alu/AdderSpec.scala
@@ -0,0 +1,41 @@
+package pext_alu
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import chisel3.experimental.BundleLiterals._
+import jdk.jfr.Unsigned
+
+import scala.util.Random
+
+class AdderSpec extends AnyFreeSpec with ChiselScalatestTester {
+  def Int64ToInt8Vec(int64: Long): IndexedSeq[Byte] = {
+    (0 until 8).map(i => ((int64 >>> (i*8)) & 0xFF).toByte)
+  }
+  def Int8Vec_Add(vec1: IndexedSeq[Byte], vec2: IndexedSeq[Byte]): IndexedSeq[Byte] = {
+    require((vec1.length == 8) && (vec2.length == 8), "no")
+    (0 until 8).map(i => (vec1(i) + vec2(i)).toByte)
+  }
+  def Int64ToBigInt(int64: Long): BigInt = {
+    (BigInt(int64 >>> 1) << 1) + (int64 & 0x1)
+  }
+
+  "SIMD Adder should not act sussy" in {
+    test(Adder(xprlen = 64)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      Random.setSeed(0)
+      val testValues_in1 = (0 until 32).map(_ => Random.nextLong)
+      val testValues_in2 = (0 until 32).map(_ => Random.nextLong)
+
+      val testValues8_inputValue1 = testValues_in1.map(x => Int64ToBigInt(x).U(64.W))
+      val testValues8_inputValue2 = testValues_in2.map(x => Int64ToBigInt(x).U(64.W))
+
+      dut.io.elen.poke("b00".U)
+      for(i <- 0 until 32) {
+        dut.io.rs1_value.poke(testValues8_inputValue1(i))
+        dut.io.rs2_value.poke(testValues8_inputValue2(i))
+        dut.clock.step()
+      }
+    }
+  }
+}
