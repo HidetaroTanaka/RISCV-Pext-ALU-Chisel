c1918d65324ee7c4c170663922002e37b752d7a1
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-06-13 12:55:00 +0900

    I asked ChatGPT about the bug where SIMD addition generates wrong value, but ChatGPT generated wrong answer! This proves me, a human race, is way much smarter than silly chat AI! Victory of Humanity! I won against ChatGPT!

--- src/main/scala/pext_alu/Adder.scala
+++ src/main/scala/pext_alu/Adder.scala
@@ -10,26 +10,38 @@ class Adder(xprlen: Int) extends Module {
     val rs2_value = Input(UInt(xprlen.W))
     val elen = Input(UInt(2.W)) // 00 -> 8bit, 01 -> 16bit, 10 -> 32bit
     val out = Output(UInt(xprlen.W))
+    val carry = Output(Bool())
   })
+
+  /**
+   * 例：{0, 0, 1, 1, 0, 0, 1, 1} => 8'b00110011
+   * @param seq BoolのSeq配列
+   * @return 入力配列を結合してUIntにしたもの
+   */
+  def SeqBoolToUInt(seq: Seq[Bool]): UInt = {
+    Cat(seq.reverse)
+  }
+
+  /**
+   * UIntを8bitSIMDベクタに変換する
+   * @param uint 元の値（64bitまたは32bit）
+   * @return uintを下位から8bitごとに分割してVec[UInt]にしたもの
+   */
+  def divideUIntToSimd8(uint: UInt): Vec[UInt] = {
+    val simd_len = uint.getWidth / 8
+    val simd8 = Wire(Vec(simd_len, UInt(8.W)))
+    // 0x01234567 => Seq(0x01, 0x23, 0x45, 0x67)
+    val int8_array = uint.asBools.grouped(8).toSeq
+    simd8.zipWithIndex.foreach(e => e._1 := SeqBoolToUInt(int8_array(e._2)))
+    simd8
+  }
 
   val e8_simd_len = xprlen / 8
   val e16_simd_len = xprlen / 16
   val e32_simd_len = xprlen / 32
-  val e8_rs1_vec = Wire(Vec(e8_simd_len, UInt(8.W)))
-  val e8_rs2_vec = Wire(Vec(e8_simd_len, UInt(8.W)))
+  val e8_rs1_vec = divideUIntToSimd8(io.rs1_value)
+  val e8_rs2_vec = divideUIntToSimd8(io.rs2_value)
   val e8_out_with_carry_vec = Wire(Vec(e8_simd_len, UInt(9.W)))
-
-  // 0x0123_4567_89AB_CDEF => {0x01, 0x23, ..., 0xEF}
-  for ((d, i) <- e8_rs1_vec.zipWithIndex) {
-    d := io.rs1_value((8-i) * 8 - 1, (7-i) * 8)
-  }
-  for ((d, i) <- e8_rs2_vec.zipWithIndex) {
-    d := io.rs2_value((8-i) * 8 - 1, (7-i) * 8)
-  }
-
-  // No readability but looks cool
-  // e8_rs1_vec.zipWithIndex.foreach(e => e._1 := io.rs1_value((8-e._2) * 8 - 1, (7-e._2) * 8))
-  // e8_rs2_vec.zipWithIndex.foreach(e => e._1 := io.rs2_value((8-e._2) * 8 - 1, (7-e._2) * 8))
 
   val e8 = (io.elen === "b00".U)
   val e16 = (io.elen === "b01".U)
@@ -48,12 +60,16 @@ class Adder(xprlen: Int) extends Module {
     d := Cat(e16_out_with_carry_vec(i*2+1) + e16_out_with_carry_vec(i*2)(16), e16_out_with_carry_vec(i*2)(15,0))
   }
 
+  io.carry := false.B
   when(e8) {
-    io.out := Cat(e8_out_with_carry_vec.map(i => i(7,0)))
+    io.out := Cat(e8_out_with_carry_vec.map(i => i(7,0)).reverse)
+    io.carry := e8_out_with_carry_vec.map(i => i(8)).reduce(_ || _)
   } .elsewhen(e16) {
-    io.out := Cat(e16_out_with_carry_vec.map(i => i(15,0)))
+    io.out := Cat(e16_out_with_carry_vec.map(i => i(15,0)).reverse)
+    io.carry := e16_out_with_carry_vec.map(i => i(16)).reduce(_ || _)
   } .otherwise {
-    io.out := (if(xprlen != 64) 0.U(xprlen.W) else Cat(e32_out_with_carry_vec.map(i => i(31,0))))
+    io.out := (if(xprlen != 64) 0.U(xprlen.W) else Cat(e32_out_with_carry_vec.map(i => i(31,0)).reverse))
+    io.carry := (if(xprlen != 64) false.B else e32_out_with_carry_vec.map(i => i(32)).reduce(_ || _))
   }
 }
 
--- src/test/scala/pext_alu/AdderSpec.scala
+++ src/test/scala/pext_alu/AdderSpec.scala
@@ -20,12 +20,23 @@ class AdderSpec extends AnyFreeSpec with ChiselScalatestTester {
   def Int64ToBigInt(int64: Long): BigInt = {
     (BigInt(int64 >>> 1) << 1) + (int64 & 0x1)
   }
+  def Int8Vec_Concanate(int8vec: IndexedSeq[Byte]): Long = {
+    require(int8vec.length == 8)
+    int8vec.zipWithIndex.map(e => (e._1.toLong & 0xFF) << (e._2 * 8)).sum
+  }
 
   "SIMD Adder should not act sussy" in {
     test(Adder(xprlen = 64)).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
       Random.setSeed(0)
-      val testValues_in1 = (0 until 32).map(_ => Random.nextLong)
-      val testValues_in2 = (0 until 32).map(_ => Random.nextLong)
+      val testValues_in1: Seq[Long] = (0 until 32).map(_ => Random.nextLong)
+      val testValues_in2: Seq[Long] = (0 until 32).map(_ => Random.nextLong)
+      val testValues_out: Seq[Long] = (0 until 32).map(
+        i => Int8Vec_Concanate(Int8Vec_Add(Int64ToInt8Vec(testValues_in1(i)), Int64ToInt8Vec(testValues_in2(i))))
+      )
+
+      println(testValues_in1.map(x => x.toHexString.toUpperCase).mkString("(", ", ", ")"))
+      println(testValues_in2.map(x => x.toHexString.toUpperCase).mkString("(", ", ", ")"))
+      println(testValues_out.map(x => x.toHexString.toUpperCase).mkString("(", ", ", ")"))
 
       val testValues8_inputValue1 = testValues_in1.map(x => Int64ToBigInt(x).U(64.W))
       val testValues8_inputValue2 = testValues_in2.map(x => Int64ToBigInt(x).U(64.W))
