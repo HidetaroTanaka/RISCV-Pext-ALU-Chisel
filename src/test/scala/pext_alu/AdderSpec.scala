c37fee0557416b1f9cfce427e72dd29a501e101e
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-06-13 17:42:30 +0900

    Generalising functions is now my new sexuality. This is caused by ChatGPT.

--- src/test/scala/pext_alu/AdderSpec.scala
+++ src/test/scala/pext_alu/AdderSpec.scala
@@ -9,6 +9,14 @@ import scala.util.Random
 import scala.reflect.runtime.universe._
 
 class AdderSpec extends AnyFreeSpec with ChiselScalatestTester {
+  def getElenOf[T: TypeTag]: Int = {
+    typeOf[T] match {
+      case c if c == typeOf[Byte] => 8
+      case c if c == typeOf[Short] => 16
+      case c if c == typeOf[Int] => 32
+      case _ => throw new Exception(s"${typeOf[T]} can't be element of 64bit SIMD register.")
+    }
+  }
   def Int64ToSimdVec[T: TypeTag](int64: Long): IndexedSeq[T] = {
     val convertToByte: (Long => Byte) = (x => x.toByte)
     val convertToShort: (Long => Short) = (x => x.toShort)
@@ -21,63 +29,48 @@ class AdderSpec extends AnyFreeSpec with ChiselScalatestTester {
       case _ => throw new Exception(s"${typeOf[T]} can't be element of 64bit SIMD register.")
     }
 
-    (0 until 64/elen).map(i => converter((int64 >>> (i*elen)) & ((1 << elen) - 1)))
+    (0 until 64/elen).map(i => converter((int64 >>> (i*elen)) & ((1L << elen) - 1)))
   }
-  def Int64ToInt8Vec(int64: Long): IndexedSeq[Byte] = {
-    (0 until 8).map(i => ((int64 >>> (i*8)) & 0xFF).toByte)
-  }
-  def Int64ToInt16Vec(int64: Long): IndexedSeq[Short] = {
-    (0 until 4).map(i => ((int64 >>> (i*16)) & 0xFFFF).toShort)
-  }
-  def Int64ToInt32Vec(int64: Long): IndexedSeq[Int] = {
-    (0 until 2).map(i => ((int64 >>> (i*32)) & 0xFFFFFFFF).toInt)
-  }
-  def Int8Vec_ApplyOp(vec1: IndexedSeq[Byte], vec2: IndexedSeq[Byte], op: (Byte, Byte) => Byte): IndexedSeq[Byte] = {
-    require((vec1.length == 8) && (vec2.length == 8), "no")
-    (0 until 8).map(i => op(vec1(i), vec2(i)))
+  def SimdVec_ApplyOp[T: TypeTag](vec1: IndexedSeq[T], vec2: IndexedSeq[T], op: (T, T) => T): IndexedSeq[T] = {
+    val elen = getElenOf[T]
+    require((vec1.length == 64/elen) && (vec2.length == 64/elen), "no")
+    (0 until 64/elen).map(i => op(vec1(i), vec2(i)))
   }
   def Int8Vec_Add(vec1: IndexedSeq[Byte], vec2: IndexedSeq[Byte]): IndexedSeq[Byte] = {
-    Int8Vec_ApplyOp(vec1, vec2, (x, y) => (x + y).toByte)
+    SimdVec_ApplyOp[Byte](vec1, vec2, (x, y) => (x + y).toByte)
   }
   def Int8Vec_Sub(vec1: IndexedSeq[Byte], vec2: IndexedSeq[Byte]): IndexedSeq[Byte] = {
-    Int8Vec_ApplyOp(vec1, vec2, (x, y) => (x - y).toByte)
-  }
-  def Int16Vec_ApplyOp(vec1: IndexedSeq[Short], vec2: IndexedSeq[Short], op: (Short, Short) => Short): IndexedSeq[Short] = {
-    require((vec1.length == 4) && (vec2.length == 4), "no")
-    (0 until 4).map(i => op(vec1(i), vec2(i)))
+    SimdVec_ApplyOp[Byte](vec1, vec2, (x, y) => (x - y).toByte)
   }
   def Int16Vec_Add(vec1: IndexedSeq[Short], vec2: IndexedSeq[Short]): IndexedSeq[Short] = {
-    Int16Vec_ApplyOp(vec1, vec2, (x, y) => (x + y).toShort)
+    SimdVec_ApplyOp[Short](vec1, vec2, (x, y) => (x + y).toShort)
   }
   def Int16Vec_Sub(vec1: IndexedSeq[Short], vec2: IndexedSeq[Short]): IndexedSeq[Short] = {
-    Int16Vec_ApplyOp(vec1, vec2, (x, y) => (x - y).toShort)
-  }
-  def Int32Vec_ApplyOp(vec1: IndexedSeq[Int], vec2: IndexedSeq[Int], op: (Int, Int) => Int): IndexedSeq[Int] = {
-    require((vec1.length == 2) && (vec2.length == 2), "no")
-    (0 until 2).map(i => op(vec1(i), vec2(i)))
+    SimdVec_ApplyOp[Short](vec1, vec2, (x, y) => (x - y).toShort)
   }
   def Int32Vec_Add(vec1: IndexedSeq[Int], vec2: IndexedSeq[Int]): IndexedSeq[Int] = {
-    require((vec1.length == 2) && (vec2.length == 2), "no")
-    (0 until 2).map(i => vec1(i) + vec2(i))
+    SimdVec_ApplyOp[Int](vec1, vec2, (x, y) => x + y)
   }
   def Int32Vec_Sub(vec1: IndexedSeq[Int], vec2: IndexedSeq[Int]): IndexedSeq[Int] = {
-    require((vec1.length == 2) && (vec2.length == 2), "no")
-    (0 until 2).map(i => vec1(i) - vec2(i))
+    SimdVec_ApplyOp[Int](vec1, vec2, (x, y) => x - y)
   }
   def Int64ToBigInt(int64: Long): BigInt = {
     (BigInt(int64 >>> 1) << 1) + (int64 & 0x1)
   }
-  def Int8Vec_Concatenate(int8vec: IndexedSeq[Byte]): Long = {
-    require(int8vec.length == 8)
-    int8vec.zipWithIndex.map(e => (e._1.toLong & 0xFF) << (e._2 * 8)).sum
-  }
-  def Int16Vec_Concatenate(int16vec: IndexedSeq[Short]): Long = {
-    require(int16vec.length == 4)
-    int16vec.zipWithIndex.map(e => (e._1.toLong & 0xFFFF) << (e._2 * 16)).sum
-  }
-  def Int32Vec_Concatenate(int32vec: IndexedSeq[Int]): Long = {
-    require(int32vec.length == 2)
-    int32vec.zipWithIndex.map(e => (e._1.toLong & 0x00000000FFFFFFFFL) << (e._2 * 32)).sum
+  def SimdVec_Concatenate[T: TypeTag](simdVec: IndexedSeq[T]): Long = {
+    val convertFromByte: (Byte => Long) = (x => x.toLong)
+    val convertFromShort: (Short => Long) = (x => x.toLong)
+    val convertFromInt: (Int => Long) = (x => x.toLong)
+
+    val elen = getElenOf[T]
+    require(simdVec.length == 64/elen)
+    val converter: T => Long = typeOf[T] match {
+      case c if c == typeOf[Byte] => convertFromByte.asInstanceOf[T => Long]
+      case c if c == typeOf[Short] => convertFromShort.asInstanceOf[T => Long]
+      case c if c == typeOf[Int] => convertFromInt.asInstanceOf[T => Long]
+      case _ => throw new Exception(s"${typeOf[T]} can't be element of 64bit SIMD register.")
+    }
+    simdVec.zipWithIndex.map(e => (converter(e._1) & ((1L << elen) - 1)) << (e._2 * elen)).sum
   }
 
   "SIMD Adder should not act sussy" in {
@@ -86,22 +79,22 @@ class AdderSpec extends AnyFreeSpec with ChiselScalatestTester {
       val testValues_in1 = (0 until 32).map(_ => Random.nextLong)
       val testValues_in2 = (0 until 32).map(_ => Random.nextLong)
       val testValues_out = (0 until 32).map(
-        i => Int8Vec_Concatenate(Int8Vec_Add(Int64ToSimdVec[Byte](testValues_in1(i)), Int64ToSimdVec[Byte](testValues_in2(i))))
+        i => SimdVec_Concatenate[Byte](Int8Vec_Add(Int64ToSimdVec[Byte](testValues_in1(i)), Int64ToSimdVec[Byte](testValues_in2(i))))
       )
       val testValues16bit_out = (0 until 32).map(
-        i => Int16Vec_Concatenate(Int16Vec_Add(Int64ToInt16Vec(testValues_in1(i)), Int64ToInt16Vec(testValues_in2(i))))
+        i => SimdVec_Concatenate[Short](Int16Vec_Add(Int64ToSimdVec[Short](testValues_in1(i)), Int64ToSimdVec[Short](testValues_in2(i))))
       )
       val testValues32bit_out = (0 until 32).map(
-        i => Int32Vec_Concatenate(Int32Vec_Add(Int64ToInt32Vec(testValues_in1(i)), Int64ToInt32Vec(testValues_in2(i))))
+        i => SimdVec_Concatenate[Int](Int32Vec_Add(Int64ToSimdVec[Int](testValues_in1(i)), Int64ToSimdVec[Int](testValues_in2(i))))
       )
       val testValuesSub8bit_out = (0 until 32).map(
-        i => Int8Vec_Concatenate(Int8Vec_Sub(Int64ToInt8Vec(testValues_in1(i)), Int64ToInt8Vec(testValues_in2(i))))
+        i => SimdVec_Concatenate[Byte](Int8Vec_Sub(Int64ToSimdVec[Byte](testValues_in1(i)), Int64ToSimdVec[Byte](testValues_in2(i))))
       )
       val testValuesSub16bit_out = (0 until 32).map(
-        i => Int16Vec_Concatenate(Int16Vec_Sub(Int64ToInt16Vec(testValues_in1(i)), Int64ToInt16Vec(testValues_in2(i))))
+        i => SimdVec_Concatenate[Short](Int16Vec_Sub(Int64ToSimdVec[Short](testValues_in1(i)), Int64ToSimdVec[Short](testValues_in2(i))))
       )
       val testValuesSub32bit_out = (0 until 32).map(
-        i => Int32Vec_Concatenate(Int32Vec_Sub(Int64ToInt32Vec(testValues_in1(i)), Int64ToInt32Vec(testValues_in2(i))))
+        i => SimdVec_Concatenate[Int](Int32Vec_Sub(Int64ToSimdVec[Int](testValues_in1(i)), Int64ToSimdVec[Int](testValues_in2(i))))
       )
 
       // println(testValues_in1.map(x => x.toHexString.toUpperCase).mkString("(", ", ", ")"))
