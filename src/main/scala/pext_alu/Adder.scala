6dfac04af0cfd4e83bd7a3db229ab780cf76dab9
HidetaroTanaka <s227891v@st.me.tuat.ac.jp>
2023-06-15 16:15:13 +0900

    amogus

--- src/main/scala/pext_alu/Adder.scala
+++ src/main/scala/pext_alu/Adder.scala
@@ -4,6 +4,8 @@ import chisel3._
 import chisel3.util._
 import chisel3.stage.ChiselStage
 
+import scala.collection.LazyZip3
+
 // TODO: I think Saturating (K) and Halving (R) can share this Adder
 
 class Adder(xprlen: Int) extends Module {
@@ -21,7 +23,6 @@ class Adder(xprlen: Int) extends Module {
     val saturating = Input(Bool())
     val halving = Input(Bool())
     val out = Output(UInt(xprlen.W))
-    val overflow = Output(Bool())
   })
 
   /**
@@ -48,29 +49,48 @@ class Adder(xprlen: Int) extends Module {
     }
     simd8
   }
+  /*
+  def saturate(uint: UInt, signed: Bool): UInt = {
+    Mux(signed,
+      MuxCase(uint, Array(
+        uint.tail(0).asBool -> Cat(false.B, Fill(uint.getWidth-1, true.B)),
+        ???
+      ))
+    )
+  }
+   */
 
   val e8_simd_len = xprlen / 8
   val e16_simd_len = xprlen / 16
   val e32_simd_len = xprlen / 32
   val e8_rs1_vec = divideUIntToSimd8(io.rs1_value)
   val e8_rs2_vec = divideUIntToSimd8(io.rs2_value)
-  val e8_out_with_carry_vec = Wire(Vec(e8_simd_len, UInt(9.W)))
+  val e8_out_as_result9 = Wire(Vec(e8_simd_len, UInt(9.W)))
+  val zippedInputsAndOutput: LazyZip3[UInt, UInt, UInt, e8_rs1_vec.type] = e8_rs1_vec.lazyZip(e8_rs2_vec).lazyZip(e8_out_as_result9)
 
   val e8 = (io.elen === "b00".U)
   val e16 = (io.elen === "b01".U)
 
-  for ((d, i) <- e8_out_with_carry_vec.zipWithIndex) {
-    d := Mux(io.addsub,
-      Cat(false.B, e8_rs1_vec(i)) - Cat(false.B, e8_rs2_vec(i)),
-      Cat(false.B, e8_rs1_vec(i)) + Cat(false.B, e8_rs2_vec(i))
+  for((in1, in2, res) <- zippedInputsAndOutput) {
+    res := Mux(!io.signed || (!io.halving && !io.saturating),
+      // Unsigned (ADD, URADD, UKADD)
+      Mux(io.addsub,
+        Cat(false.B, in1) - Cat(false.B, in2),
+        Cat(false.B, in1) + Cat(false.B, in2)
+      ),
+      // Signed
+      Mux(io.addsub,
+        Cat(in1.tail(0), in1) - Cat(in2.tail(0), in2),
+        Cat(in1.tail(0), in1) + Cat(in2.tail(0), in2)
+      )
     )
   }
 
   val e16_out_with_carry_vec = Wire(Vec(e16_simd_len, UInt(17.W)))
   for((d,i) <- e16_out_with_carry_vec.zipWithIndex) {
     d := Mux(io.addsub,
-      Cat(e8_out_with_carry_vec(i*2+1) - e8_out_with_carry_vec(i*2)(8), e8_out_with_carry_vec(i*2)(7,0)),
-      Cat(e8_out_with_carry_vec(i*2+1) + e8_out_with_carry_vec(i*2)(8), e8_out_with_carry_vec(i*2)(7,0))
+      Cat(e8_out_as_result9(i*2+1) - e8_out_as_result9(i*2)(8), e8_out_as_result9(i*2)(7,0)),
+      Cat(e8_out_as_result9(i*2+1) + e8_out_as_result9(i*2)(8), e8_out_as_result9(i*2)(7,0))
     )
   }
 
@@ -82,9 +102,8 @@ class Adder(xprlen: Int) extends Module {
     )
   }
 
-  io.overflow := false.B
   when(e8) {
-    io.out := Cat(e8_out_with_carry_vec.map(i => i(7,0)).reverse)
+    io.out := Cat(e8_out_as_result9.map(i => i(7,0)).reverse)
     // io.carry := e8_out_with_carry_vec.map(i => i(8)).reduce(_ || _)
   } .elsewhen(e16) {
     io.out := Cat(e16_out_with_carry_vec.map(i => i(15,0)).reverse)
